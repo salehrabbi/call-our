2d2eca959d5626f7b701fea6577db9c6eec288ce
Saleh Rabbi <33930388+salehrabbi@users.noreply.github.com>
2019-12-19 13:27:29 +0600

    education

---
+++ resources/assets/script/custom.js
@@ -0,0 +1,44 @@
+$("#slice-4-tigger").hover(
+  function() {
+    $("#slice-4").addClass("hover");
+  },
+  function() {
+    $("#slice-4").removeClass("hover");
+  }
+);
+
+$("#slice-5-tigger").hover(
+  function() {
+    $("#slice-5").addClass("hover");
+  },
+  function() {
+    $("#slice-5").removeClass("hover");
+  }
+);
+
+$("#slice-1-tigger").hover(
+  function() {
+    $("#slice-1").addClass("hover");
+  },
+  function() {
+    $("#slice-1").removeClass("hover");
+  }
+);
+
+$("#slice-2-tigger").hover(
+  function() {
+    $("#slice-2").addClass("hover");
+  },
+  function() {
+    $("#slice-2").removeClass("hover");
+  }
+);
+
+$("#slice-3-tigger").hover(
+  function() {
+    $("#slice-3").addClass("hover");
+  },
+  function() {
+    $("#slice-3").removeClass("hover");
+  }
+);
